d4e764bbd3ea035e4349dcdf619aa24d25c066b8
cyoo1 <yoo.chrisx15@gmail.com>
2023-04-20 11:56:09 -0600

    fixed idle app check and moved from App.js to index.html

--- App.js
+++ App.js
@@ -3,22 +3,12 @@ import { StatusBar } from 'expo-status-bar';
 import { AppState, StyleSheet, SafeAreaView, Keyboard, View} from 'react-native';
 import { WebView } from 'react-native-webview';
 import AddrForm from './components/AddrForm.js';
-// import { usb } from 'usb';
 import htm from './src/index.html';
 
 export default function App() {
   const [content, setContent] = useState("");
   const [addr, setAddr] = useState("");
   const [submitted, setSubmitted] = useState(false);
-  //const appState = useRef(AppState.currentState);
-  // useEffect(() => {
-  //   const appStateListen = AppState.addEventListener('change', nextAppState => {
-  //       if(nextAppState != 'active') {
-  //         socket.disconnect();
-  //         console.log("client disconnected - app left")
-  //       }
-  //     })
-  // })
 
   const submit = async () => {
     if (submitted) return;
